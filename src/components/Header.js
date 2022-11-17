02cc9b1273b09322747ea74a099ca644339bb28b
Nimra Ghazal <Nimraghazal19@gmail.com>
2022-11-17 18:15:39 +0500

    search bar state update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Container, Navbar } from "react-bootstrap";
 
+const navbarStyle = {
+  backgroundColor: "lightblue",
+};
 const Header = (props) => {
   // const { title } = props; //destructuring the propererties for further action
   return (
-    <Navbar bg="dark" variant="dark">
+
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
       <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+        
+      </Container>
     </Navbar>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { Form, Button, Container, Col, Row } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({ word, setWord, handleSubmit }) => {
   return (
-    <Container>
-      <Form>
-        <Row>
-          <Col>
-            <Form.Control placeholder="Search images  here" />
-          </Col>
-          <Col>
-            {" "}
-            <Button variant="primary" type="submit">
-              Search
-            </Button>
-          </Col>
-        </Row>
-      </Form>
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col xs={9}>
+                <Form.Control
+                  type="text"
+                  vlaue={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search images  here" />
+
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+
+      </Row>
+
     </Container>
   );
 };
